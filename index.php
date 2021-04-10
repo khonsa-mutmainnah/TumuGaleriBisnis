933ebcc0fc89ec19563c0495cd593175e13eb346
khonsa-mutmainnah <khonsamutmainnah@gmail.com>
2021-04-10 22:55:09 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Tumu Galeri Bisnis</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" type="text/css" href="StyleIndex.css">
+
+    <!-- BOOTSTRAP CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+    <!-- BOOTSTRAP JS -->
+    <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+        crossorigin="anonymous"
+    ></script>
+    
+    <!-- font -->
+    <style>@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;600;700;800;900&display=swap');</style>
+    
+</head>
+<body class="all">
+    <div class="header">
+        <nav class="navbar navbar-expand-lg">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="?p=Beranda"><img src="gambar/logo.png" alt="logo" style="width: 60px;"></a>
+                <div judul-navbar>
+                    <h3 class="galeribisnis-navbar">galeri bisnis</h3>
+                    <div class="tagline-navbar">tunjukkan bisnismu di sini!</div>
+                </div>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="?p=Beranda">
+                            <img src="gambar/toko.png" alt="toko" style="width: 30px;">
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false"><img src="gambar/notif.png" alt="notif" style="width: 30px;"></a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item" href="#"></a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="akun-dropdown" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <img src="gambar/aa.jpg" alt="toko" style="width: 30px;">
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item" href="#">Edit Profil</a></li>
+                            <li><a class="dropdown-item" href="#">Tambah Produk</a></li>
+                            <li><a class="dropdown-item" href="#">Keluar</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+    <div class="isi">
+        <?php
+            $pages_dir = 'pages';
+            if (!empty($_GET['p'])) {
+                $pages = scandir($pages_dir, 0);
+                unset($pages[0], $pages[1]);
+
+                $p = $_GET['p'];
+
+                if (in_array($p . '.php', $pages)) {
+                    include($pages_dir . '/' . $p .  '.php');
+                } else {
+                    echo "Halaman Tidak Ditemukan";
+                }
+            } 
+            else {
+                include "./pages/home.php";
+            }
+        ?>
+    </div>
+</body>
+</html>
---
+++ pages/Home.php
@@ -0,0 +1,10 @@
+<div class="Home">
+    <h5 class="sel-main">selamat datang di</h5>
+    <h1 class="tumu-main">TUMU Galeri Bisnis</h1>
+    <div class="tagline-main">tunjukkan bisnismu di sini!</div>
+                
+    <div class="tekan">
+        <a class="pengunjung" href="?p=Pengunjung"><button>Pengunjung</button></a>
+        <a class="penjual" href="LoginTumu.html"><button>Penjual</button></a>
+    </div>
+</div>
