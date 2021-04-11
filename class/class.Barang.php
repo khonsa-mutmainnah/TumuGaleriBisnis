805a706748466b65be35e4dba8583524a2f079b2
AbdurrahamanFaqih01 <abdurrahamanfaqih11@gmail.com>
2021-04-11 15:33:48 +0700

    Menambahkan di class.barang.php

--- class/class.Barang.php
+++ class/class.Barang.php
@@ -1,2 +1,40 @@
 <?php 
+
+class User{
+    private $kode;
+    private $namaBarang;
+    private $deskripsi;
+    private $harga;
+    private $variasi;
+    private $gambar;
+    private $status;
+
+    //User Contructor
+    public function __construct(
+        $kode;
+        $namaBarang;
+        $deskripsi;
+        $harga;
+        $variasi;
+        $gambar;
+        $status;
+    )
+    {
+        $this->kode = $kode;
+        $this->namaBarang = $namaBarang;
+        $this->deskripsi = $deskripsi;
+        $this->harga = $harga;
+        $this->variasi = $variasi;
+        $this->gambar = $gambar;
+        $this->status = $status;
+    }
+
+    //Get Automatic
+    public function __get($atribute){
+        if(property_exists($this, $atribute))
+        {
+            return $this->$atribute;
+        }
+    }
+}
 ?>
