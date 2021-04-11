e7f5656190c6ca17c5997e5ada8082dc11cb6051
AbdurrahamanFaqih01 <abdurrahamanfaqih11@gmail.com>
2021-04-11 15:26:46 +0700

    Menambah atribute di class.user

--- class/class.User.php
+++ class/class.User.php
@@ -19,5 +19,22 @@ class User{
         $kota;
         $role;
     )
+    {
+        $this->username = $username;
+        $this->password = $password;
+        $this->nama = $nama;
+        $this->noTelp = $noTelp;
+        $this->email = $email;
+        $this->kota = $kota;
+        $this->role = $role;
+    }
+
+    //Get Automatic
+    public function __get($atribute){
+        if(property_exists($this, $atribute))
+        {
+            return $this->$atribute;
+        }
+    }
 }
 ?>
