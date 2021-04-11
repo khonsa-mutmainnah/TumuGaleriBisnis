fa96d49d26367ac4b4da703cf6ed59d9faf830cb
AbdurrahamanFaqih01 <abdurrahamanfaqih11@gmail.com>
2021-04-11 15:14:23 +0700

    menambahkan class

---
+++ class/class.User.php
@@ -0,0 +1,23 @@
+<?php 
+
+class User{
+    private $username;
+    private $password;
+    private $nama;
+    private $noTelp;
+    private $email;
+    private $kota;
+    private $role;
+
+    //User Contructor
+    public function __construct(
+        $username;
+        $password;
+        $nama;
+        $noTelp;
+        $email;
+        $kota;
+        $role;
+    )
+}
+?>
