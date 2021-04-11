e2db7fdb0ad8b780585217dff221c8fe7c3dff5b
AbdurrahamanFaqih01 <abdurrahamanfaqih11@gmail.com>
2021-04-11 15:48:38 +0700

    Menambahkan atribute di class.toko.php

--- class/class.Toko.php
+++ class/class.Toko.php
@@ -1,2 +1,43 @@
 <?php 
+
+class Toko{
+    private $idToko;
+    private $namaToko;
+    private $logo;
+    private $alamat;
+    private $tagline;
+    private $noTelp;
+    private $status;
+    private $website;
+
+    //User Contructor
+    public function __construct(
+        $idToko;
+        $namaToko;
+        $logo;
+        $alamat;
+        $tagline;
+        $noTelp;
+        $status;
+        $website;
+    )
+    {
+        $this->idToko = $idToko;
+        $this->namaToko = $namaToko;
+        $this->logo = $logo;
+        $this->alamat = $alamat;
+        $this->tagline = $tagline;
+        $this->noTelp = $noTelp;
+        $this->status = $status;
+        $this->website = $website;
+    }
+
+    //Get Automatic
+    public function __get($atribute){
+        if(property_exists($this, $atribute))
+        {
+            return $this->$atribute;
+        }
+    }
+}
 ?>
