70bb2da60fda2cc1f9c564565509c036fd53ebef
ESQ-Avrijsto <a.amandri.a@students.esqbs.ac.id>
2021-04-18 22:05:40 +0700

    Masukin index navbar sama tambah produk

--- index.php
+++ index.php
@@ -15,13 +15,25 @@
         integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
         crossorigin="anonymous"
     ></script>
+
+     <!-- Latest compiled and minified CSS -->
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+        <!-- Popper JS -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script> 
     
     <!-- font -->
     <style>@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;600;700;800;900&display=swap');</style>
     
 </head>
 <body class="all">
-    <div class="header">
+    <!-- <div class="header">
         <nav class="navbar navbar-expand-lg">
             <div class="container-fluid">
                 <a class="navbar-brand" href="?p=Beranda"><img src="gambar/logo.png" alt="logo" style="width: 60px;"></a>
@@ -61,7 +73,47 @@
                 </div>
             </div>
         </nav>
-    </div>
+    </div> -->
+    <div class="header">
+            <nav class="navbar navbar-expand-lg navbar-light">
+                    <a class="navbar-brand" href="?p=Beranda"><img src="./gambar/logo.png" alt="logo" style="width:85px; margin-left:20px;"></a>
+                    <div class="judul-navbar">
+                        <h3 class="galeribisnis-navbar">galeri bisnis</h3>
+                        <div class="tagline-navbar">tunjukkan bisnismu di sini!</div>
+                    </div>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse " id="navbarSupportedContent">
+                        <ul class="navbar-nav ml-auto justify-content-end">
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <img src="./gambar/notif.png" style="height:30px;" alt="">
+                                </a>
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Another action</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="?p=tambahproduk">Something else here</a>
+                                </div>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <img src="./gambar/toko.png" style="height:30px;" alt="">
+                                </a>
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Another action</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="#">Something else here</a>
+                                </div>
+                            </li>
+                            
+                        </ul>
+                    </div>
+            </nav>
+        </div>
     <div class="isi">
         <?php
             $pages_dir = 'pages';
