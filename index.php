7e3ec6b899d9c946d4a22ad67ed5bfca288db028
khonsa-mutmainnah <khonsamutmainnah@gmail.com>
2021-05-19 20:25:47 +0700

    tambah

--- index.php
+++ index.php
@@ -54,7 +54,7 @@ require "connection.php";
                         <a class="nav-link " style="color: #002f3f;"href="#">+Produk</a>
                         <a class="nav-link " style="color: #002f3f;"href="?p=login">login</a>
                         <a class="nav-link " style="color: #002f3f;"href="?p=register-user">reg</a>
-                        <a class="nav-link " style="color: #002f3f;"href="?p=user">user</a>
+                        <a class="nav-link " style="color: #002f3f;"href="?p=GaleriBisnis">user</a>
                         <div class="btn-group">
                             <button type="button" class="btn" data-bs-display="static" data-bs-toggle="dropdown" aria-expanded="false">
                                 <img src="./gambar/aa.jpg" style="width:35px; border-radius:50%;">
