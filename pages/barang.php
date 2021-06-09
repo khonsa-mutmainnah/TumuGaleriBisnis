c4d925cea335fe8d74145f7c64eaf5510e2c074e
AbdurrahamanFaqih01 <abdurrahamanfaqih11@gmail.com>
2021-06-10 04:04:34 +0700

    Add CSS to form barang

--- pages/barang.php
+++ pages/barang.php
@@ -4,6 +4,7 @@
 
 	$objBarang = new Barang();
 	$objToko = new Toko();
+	$objTokoList= $objToko->SelectAllToko();
 
 	if (isset($_POST['btnSubmit'])) {
 		$objBarang->id_barang = $_POST['id_barang'];
@@ -30,8 +31,49 @@
 		$objBarang->SelectSatuBarang();
 	}
 ?>
+<div class="barang">
+    <div class="container col-lg-7">
+        <form form action="" method="POST" class="barang-form" enctype="multipart/form-data">
+            <h4 class="title text-center fs-1 fw-bolder" >Barang</h4>
+    
+            <div class="col">
+                <label for="exampleFormControlInput1" class="form-label">Nama Barang</label>
+                <input type="text" class="form-control" name="nama_barang" value="<?php echo $objBarang->nama_barang; ?>" placeholder="nama barang" class="form-control">
+            </div>
+            <div class="col">
+                <label for="exampleFormControlInput1" class="form-label">Deskripsi</label>
+                <input type="textarea" name="deskripsi" value="<?php echo $objBarang->deskripsi; ?>" class="form-control" id="exampleFormControlInput1" placeholder="deskripsi">
+            </div>
+            <div class="col">
+                <label for="exampleFormControlInput1" class="form-label">Harga</label>
+                <input type="text" name="harga" value="<?php echo $objBarang->harga; ?>" class="form-control" id="exampleFormControlInput1" placeholder="harga">
+            </div>
+            <div class="col">
+                <label for="exampleFormControlInput1" class="form-label">Variasi</label>
+                <input type="text" name="variasi" value="<?php echo $objBarang->variasi; ?>" class="form-control" id="exampleFormControlInput1" placeholder="variasi">
+            </div>
+            <div class="col">
+                <label for="exampleFormControlInput1" class="form-label">id_toko</label>
+                <select name="id_toko" class="form-control">
+                    <option value="">--Please select Toko--</option>
+                        <?php
+                            foreach ($objTokoList as $toko){
+                                if($objToko->toko->id_toko == $toko->id_toko)
+                                    echo '<option selected="true" value='.$toko->id_toko.'>'.$toko->nama_toko.'</option>';
+                                else
+                                    echo '<option value='.$toko->id_toko.'>'.$toko->nama_toko.'</option>';
+                            }
+                        ?>
+                </select>
+            </div>
+			<div class="col-lg-6 button-end">
+                <input class="btn btnsuccess" name="btnSubmit" type="submit" value="Save" >
+            </div>
+        </form>
+    </div>
+</div>
 
-	<h4 class="title">
+	<!-- <h4 class="title">
 		<span class="text"><strong>Barang</strong></span>
 	</h4>
 	<form action="" method="post">
@@ -86,13 +128,14 @@
 			?>
 			</select>
 		</td>
-		</tr>
-			<tr>
+		</tr> -->
+		
+			<!-- <tr>
 				<td colspan="2"></td>
 				<td><input type="submit" class="btn btnsuccess" value="Save" name="btnSubmit">
 					<a href="dashboard.php?p=baranglist" class="btn btnwarning">Cancel</a>
 				</td>
 			</tr>
 		</table>
-	</form>
+	</form> -->
 
